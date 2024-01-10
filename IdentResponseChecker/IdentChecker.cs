b897e8a7664893b5f4003c33a6d0d47dae1f0e09
PaJuS <pajus1337@gmail.com>
2024-01-10 19:55:20 +0100

    Add project files.

---
+++ IdentResponseChecker/IdentChecker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentResponseChecker
+{
+    public class IdentChecker
+    {
+        public string ServerIP { get; private set; }
+        public int Port { get; private set; }
+
+        public IdentChecker(string serverIp, int port)
+        {
+            ServerIP = serverIp;
+            Port = port;
+        }
+
+        public string TryGetIdentResponse()
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    client.Connect(ServerIP, Port);
+                    string messageToSend = "0 , 0\r\n";
+                    byte[] data = Encoding.ASCII.GetBytes(messageToSend);
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Send : " + messageToSend);
+                    byte[] buffer = new byte[256];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytes);
+
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
+    
+
---
+++ IdentResponseChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentResponseChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beta Version Test : ");
+            IdentChecker identChecker = new IdentChecker("127.0.0.1",113);
+            Console.WriteLine(identChecker.TryGetIdentResponse());
+        }
+    }
+}
